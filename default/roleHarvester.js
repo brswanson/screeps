f2668bc6c9e1eb99a397f2345dd5ace409ca5081
Ben Swanson <brswanson@github.com>
2017-12-22 16:34:33 -0500

    Harvesters now ignore sources without any energy to harvest.

--- default/roleHarvester.js
+++ default/roleHarvester.js
@@ -8,7 +8,9 @@ var roleHarvester = {
         // Harvests from the nearest source
         // TODO: Load balancing by active miners on the source
         if (creep.carry.energy < creep.carryCapacity) {
-            var source = creep.pos.findClosestByRange(FIND_SOURCES);
+            var source = creep.pos.findClosestByRange(FIND_SOURCES, {
+                filter: function (s) { return s.energy > 0 }
+            });
 
             if (creep.harvest(source) == ERR_NOT_IN_RANGE)
                 creep.moveTo(source);
@@ -17,9 +19,7 @@ var roleHarvester = {
         // TODO: Load balance by screeps with same destination
         else {
             var destination = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: function (s) {
-                    return s.energy < s.energyCapacity && (s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_EXTENSION)
-                }
+                filter: function (s) { return s.energy < s.energyCapacity && (s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_EXTENSION) }
             });
 
             if (creep.transfer(destination, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
