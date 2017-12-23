83d9121f2eb825b5dd7b2555b2e8bb6989c7063b
Ben Swanson <brswanson@github.com>
2017-12-22 23:27:34 -0500

    Removed no longer needed loop for wiping creep memory. Only needed to run once to allow creeps to be assigned jobs with a clean slate.

--- default/main.js
+++ default/main.js
@@ -7,11 +7,6 @@ module.exports.loop = function () {
     const AISpawner = require('aiSpawner');
     const GarbageCollector = require('garbageCollector');
 
-    for (var i in Game.creeps) {
-        var creep = Game.creeps[i];
-        creep.memory.job = undefined;
-    }
-
     // Iterate over all existing spawns/rooms
     // TODO: Will need to select unique rooms in the future but for now we only have one spawn per room so this works
     for (var i in Game.spawns) {
