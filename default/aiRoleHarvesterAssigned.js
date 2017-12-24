6696d69c183dea1457073c1759055b8bcc33b9a2
Ben Swanson <brswanson@github.com>
2017-12-24 03:22:10 -0500

    Adding caching of Harvester locations for rooms. Refactored Harvester AI to allocate Harvesters according to location without shuffling assigned locations. Additionally created a bug where extra miners will be created over the maximum derived from sources in a given room and/or miners will swap designated mining sources.

--- default/aiRoleHarvesterAssigned.js
+++ default/aiRoleHarvesterAssigned.js
@@ -9,28 +9,43 @@ var aiRoleHarvesterAssigned = {
             filter: function (s) { return s.memory.class === global.ClassCivilain && (s.memory.job === RoleName || s.memory.job === undefined) }
         });
 
-        // TODO: Allocate creeps such that dying creeps do not disrupt or shuffle the allocation of existing creeps
         var sources = room.find(FIND_SOURCES_ACTIVE);
-        var sourceMax = global.Utilities.hashLength(sources);
-        var sourceIndex = 0;
-        var creepCount = 0;
 
-        for (var i in creeps) {
-            // TODO: Cache this in memory instead of calculating it each run
-            var harvestingSites = global.Utilities.findAvailableMiningLocations(sources[sourceIndex]).length;
-            
-            if (creepCount === harvestingSites) {
-                sourceIndex++;
-                creepCount = 0;
+        // Cache the current room's sources then return them
+        var roomSources = cacheRoomSources(room);
+
+        // Update current worker counts for all sources
+        for (var s in roomSources) {
+            roomSources[s].capacityUsed = creeps.filter(c => c.memory.sourceId === roomSources[s].id).length;
+        }
+
+        // TODO: Fix the bug causing one harvester to be allocated than needed
+        // Assign source ids to unassigned creeps
+        for (var c in creeps.filter(c => c.memory.sourceId === undefined)) {
+            var creep = creeps[c];
+
+            for (var s in roomSources) {
+                var source = roomSources[s];
+
+                if (source.capacityUsed < source.capacity) {
+                    creep.memory.sourceId = roomSources[s].id
+                    source.capacityUsed += 1;
+
+                    break;
+                }
             }
-            if (sourceIndex >= sourceMax)
-                break;
+        }
 
-            assignRole(creeps[i], sources[sourceIndex]);
-            creepCount++;
+        // Iterate over all creeps, assigning then setting them to work as needed
+        for (var c in creeps.filter(c => c.memory.job === RoleName && c.memory.sourceId !== undefined)) {
+            var creep = creeps[c];
+            var source = sources.find(s => s.id === creep.memory.sourceId);
+
+            assignRole(creep, source);
         }
     }
 }
+
 function assignRole(creep, source) {
     Role.run(creep, source);
 
@@ -38,6 +53,20 @@ function assignRole(creep, source) {
         creep.say(RoleSymbol);
 
     // console.log('[' + room.name + '] ' + creepCount + '/' + MAX_WORKERS + ' active ' + RoleName);
+}
+
+function cacheRoomSources(room) {
+    var memory = Memory.rooms[room.name];
+
+    if (memory.sources === undefined) {
+        memory.sources = room.find(FIND_SOURCES_ACTIVE);
+
+        for (var i in memory.sources) {
+            memory.sources[i].capacity = global.Utilities.findAvailableMiningLocations(memory.sources[i]).length;
+        }
+    }
+
+    return memory.sources;
 }
 
 module.exports = aiRoleHarvesterAssigned;
